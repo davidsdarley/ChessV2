8dd52906790b56954b47b1fe9a9266b1711623fa
David <davidsdarley@gmail.com>
2025-03-03 13:17:14 -0700

    progress

---
+++ server/src/main/java/server/RegisterRequest.java
@@ -0,0 +1,12 @@
+package server;
+
+public class RegisterRequest {
+    //RegisterRequests have username, password, and email.
+    String username;
+    String password;
+    String email;
+
+    public RegisterRequest(){
+
+    }
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -5,15 +5,23 @@ import com.google.gson.Gson;
 
 public class Server {
 
-    public int run(int desiredPort) {
+
+    public static void main(String[] args) {
+        Server chessServer = new Server();
+        int port = chessServer.run(8080);
+        System.out.println(port);
+        //chessServer.stop();
+        //System.out.println("Stopped");
+
+    }
+        public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        Spark.get("/hello", Server::handleHello);
+        Spark.post("/user", Server::handleRegister);
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -23,4 +31,11 @@ public class Server {
         Spark.stop();
         Spark.awaitStop();
     }
+
+    private static Object handleHello(RegisterRequest req, Response res) {
+        return "Hello chess players!";}
+    private static Object handleRegister(RegisterRequest req, Response res) {
+        //to register, I receive a JSON dictionary with username, password, and email. I return JSON username and authtoken.
+        var details = new Gson().fromJson(req.body(), )
+        return "Hello chess players!";}
 }
