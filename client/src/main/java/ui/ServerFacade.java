1c75517baedfc664df4a6d563e112fdfbbcbb3d9
David <davidsdarley@gmail.com>
2025-03-25 16:41:32 -0600

    Logged out state finished

---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,86 @@
+package ui;
+
+import server.carriers.*;
+import java.io.IOException;
+import java.net.http.*;
+import java.net.URI;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest;
+import com.google.gson.Gson;
+
+
+public class ServerFacade {
+    HttpClient client;
+    String site;
+    public ServerFacade(String site){
+        client = HttpClient.newHttpClient();
+        this.site = site;
+    }
+    public ServerFacade(){
+        client = HttpClient.newHttpClient();
+        this.site = "http://localhost:8080";
+    }
+    public Object hello(){ //testing purposes
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(site+"/hello"))
+                .GET()
+                .build();
+        try{
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        }
+
+        catch (IOException | InterruptedException e) {
+            return false;
+        }
+    }
+    public String login(String username, String password){
+        Gson gson = new Gson();
+        LoginRequest login = new LoginRequest(username, password);
+
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(site+"/session"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(login)))
+                .build();
+        try{
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200){
+                return response.body();
+            }
+            else{
+                return "Unauthorized";
+            }
+        }
+        catch (IOException | InterruptedException e) {
+            return "Error! We're so sorry your login has failed";
+        }
+    }
+    public String register(String username, String password, String email){
+        Gson gson = new Gson();
+        RegisterRequest register = new RegisterRequest(username, password, email);
+
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(site+"/user"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(register)))
+                .build();
+
+        try{
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int code = response.statusCode();
+
+            if(code == 400){
+                return "bad request";
+            }
+            else if(code == 403){
+                return "already taken";
+            }
+            else{
+                return response.body();
+            }
+        }
+
+        catch (IOException | InterruptedException e) {
+            return "Error! We're so sorry your registration has failed";
+        }
+    }
+}
--- client/src/main/java/ui/UserInterface.java
+++ client/src/main/java/ui/UserInterface.java
@@ -1,22 +1,115 @@
 package ui;
 
+import com.google.gson.Gson;
 import server.Server;
+import server.carriers.*;
+
 import java.util.Scanner;
 
 public class UserInterface {
     Server server;
     Scanner scanner;
-    ClientRequester client;
+    ServerFacade client;
     String auth;
+    String state;
     public UserInterface(Server server) {
         //either connect to the server or make a new one
         this.server = server;
         scanner = new Scanner(System.in);
-        client = new ClientRequester();
+        client = new ServerFacade();
+    }
+    public void run(){
+        System.out.println("Welcome to Chess! Type Help to get started");
+        state = "LOGGED_OUT";
+        String input;
+        while ( !(state.equals("QUIT")) ){
+            System.out.print(state +" >>> ");
+            input = scanner.nextLine().toUpperCase();
+            performOperation(input);
+        }
+    }
+    private void register(){
+        System.out.print("Please enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Please enter password: ");
+        String password = scanner.nextLine();
+        System.out.print("Please enter email: ");
+        String email = scanner.nextLine();
+
+        String response = client.register(username, password, email);
+
+        if (response.equals("")){
+            System.out.println("Error! We're so sorry your registration has failed");
+        }
+        else if(response.equals("bad request")|| response.equals("already taken")){
+            System.out.println(response);
+        }
+        else{
+            System.out.println("Registration Successful!");
+            LoginResult login = new Gson().fromJson(response, LoginResult.class);
+            System.out.println(response);
+            auth = login.getAuthToken();
+            state = "LOGGED_IN";
+        }
+    }
+    private void login(){
+        System.out.print("Please enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Please enter password: ");
+        String password = scanner.nextLine();
+
+        String response = client.login(username, password);
+
+        if (response.equals("Unauthorized") || response.equals("Error! We're so sorry your login has failed")){
+            System.out.println("Error! We're so sorry your registration has failed");
+        }
+        else{
+            LoginResult login = new Gson().fromJson(response, LoginResult.class);
+            auth = login.getAuthToken();
+            System.out.println(auth);
+            state = "LOGGED_IN";
+        }
+    }
+
+    private void performOperation(String input){
+        if (input.equals("QUIT")){
+            state = input;
+        }
+        else if(state == "LOGGED_OUT"){
+            if (input.equals("HELP")){
+                System.out.println("   register <USERNAME> <PASSWORD> <EMAIL>");
+                System.out.println("   login <USERNAME> <PASSWORD>");
+                System.out.println("   quit");
+                System.out.println("   help");
+            }
+            else if(input.equals("REGISTER")){
+                register();
+            }
+            else if (input.equals("LOGIN")){
+                login();
+            }
+            else{
+                System.out.println("Invalid input. Type Help to see available commands");
+            }
+        }
+
+        else if(state == "LOGGED_IN"){
+            if(true){
+                System.out.println("Good job logging in!");
+            }
+            else{
+                System.out.println("Invalid input. Type Help to see available commands");
+            }
+        }
+
+
+        else{
+            System.out.println("Invalid input. Type Help to see available commands");
+        }
     }
 
     public static void main(String[] args) {
     UserInterface ui = new UserInterface(new Server());
-    System.out.println(ui.client.hello());
+    ui.run();
     }
 }
